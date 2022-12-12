e2e7354f5a95e043a0bc25013df094f6ec20be9e
Ahmet Kadir Zengin <shadowoffategame@gmail.com>
2022-12-12 06:45:14 +0300

    Added some items and UI changes
    Added some items and UI changes

---
+++ Assets/Scripts/QuestionSelection1.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class QuestionSelection1 : MonoBehaviour // Yönlü açýlar
+{
+    [SerializeField] private Questions questions;
+    public void QuestionSelection(int randomQ)
+    {
+        if (randomQ == 1)
+        {
+            questions.Question.sprite = Resources.Load<Sprite>("Enemies/teacher");
+            questions.correctAnswer = "9";
+            questions.choiceA = "6";
+            questions.choiceB = "7";
+            questions.choiceC = "8";
+            questions.choiceD = "9";
+            questions.choiceE = "10";
+        }
+        else if (randomQ == 2)
+        {
+            questions.Question.sprite = Resources.Load<Sprite>("Enemies/teacher");
+            questions.correctAnswer = "sinx";
+            questions.choiceA = "cotx";
+            questions.choiceB = "secx";
+            questions.choiceC = "cosecx";
+            questions.choiceD = "sinx";
+            questions.choiceE = "cosx";
+        }
+        else if (randomQ == 3)
+        {
+            questions.Question.sprite = Resources.Load<Sprite>("Enemies/teacher");
+            questions.correctAnswer = "-3/2";
+            questions.choiceA = "-2/3";
+            questions.choiceB = "-3/2";
+            questions.choiceC = "1/3";
+            questions.choiceD = "2/5";
+            questions.choiceE = "3/7";
+        }
+    }
+}
